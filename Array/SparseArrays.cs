c4bf87c3315e2f1b20ac9a0013e8759cecb76489
liuderwhat <33472364+liuderwhat@users.noreply.github.com>
2022-04-26 18:45:34 +0800

    dicitonary 算頻率

---
+++ Array/SparseArrays.cs
@@ -0,0 +1,90 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'matchingStrings' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY strings
+     *  2. STRING_ARRAY queries
+     */
+
+    public static List<int> matchingStrings(List<string> strings, List<string> queries)
+    {
+
+        List<int> ans = new List<int>();
+        Dictionary<string, int> freq = new Dictionary<string, int>();
+        foreach (string qry in queries)
+        {
+            if (!freq.ContainsKey(qry))
+            {
+                freq.Add(qry, 0);
+            }
+
+        }
+        foreach (string str in strings)
+        {
+            if (freq.ContainsKey(str))
+            {
+                freq[str]++;
+            }
+            
+        }
+        foreach(string q in queries)
+        {
+            ans.Add(freq[q]);
+        }
+
+        return ans;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        
+        int stringsCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> strings = new List<string>();
+
+        for (int i = 0; i < stringsCount; i++)
+        {
+            string stringsItem = Console.ReadLine();
+            strings.Add(stringsItem);
+        }
+
+        int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> queries = new List<string>();
+
+        for (int i = 0; i < queriesCount; i++)
+        {
+            string queriesItem = Console.ReadLine();
+            queries.Add(queriesItem);
+        }
+
+        List<int> res = Result.matchingStrings(strings, queries);
+        
+        foreach( int ele in res)
+        {
+            Console.WriteLine(ele);
+        }
+    }
+}
